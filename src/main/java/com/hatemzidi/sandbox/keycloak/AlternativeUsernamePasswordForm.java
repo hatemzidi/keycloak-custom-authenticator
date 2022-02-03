4f720c567896acf496dc30dcc379f9d022f1f860
hatem.zidi <hatem.zidi@gmail.com>
2022-02-03 11:43:11 +0100

    dev() : - identify the executor in the logs

--- src/main/java/com/hatemzidi/sandbox/keycloak/AlternativeUsernamePasswordForm.java
+++ src/main/java/com/hatemzidi/sandbox/keycloak/AlternativeUsernamePasswordForm.java
@@ -18,29 +18,29 @@ public class AlternativeUsernamePasswordForm extends UsernamePasswordForm implem
         MultivaluedMap<String, String> formData = context.getHttpRequest().getDecodedFormParameters();
 
         if (formData.containsKey("cancel")) {
-            log.debug("Form was canceled.");
+            log.debug("alternative-username-password-form : Form was canceled.");
             context.cancelLogin();
             return;
         }
 
         if (!validateForm(context, formData)) {
-            log.debug("Oops! User is not valid : wrong credentials or unknown");
+            log.debug("alternative-username-password-form : Oops! User is not valid : wrong credentials or unknown");
 
             boolean isRequired = context.getExecution().isRequired();
             if (!isRequired) {
-                log.debug("Execution is not required. Continuing the execution flow to the next 'alternative' task.");
+                log.debug("alternative-username-password-form : Execution is not required. Continuing the execution flow to the next 'alternative' task.");
                 context.attempted();
                 return;
             }
 
-            log.debug("Resetting the flow, all stops here.");
+            log.debug("alternative-username-password-form : Resetting the flow, all stops here.");
             context.getEvent().error(Errors.USER_NOT_FOUND);
             context.failure(AuthenticationFlowError.UNKNOWN_USER);
             return;
 
         }
 
-        log.debug("User is valid and authenticated.");
+        log.debug("alternative-username-password-form : User is valid and authenticated.");
         context.success();
     }
 }
